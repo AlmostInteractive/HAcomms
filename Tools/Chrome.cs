cf5bf94f0d0179f6c1a4298ffaf7f807c3efbf8c
Steven J <3893631+AlmostInteractive@users.noreply.github.com>
2024-07-22 23:28:04 -0600

    Added a cache to increase efficiency.  Added WE matching.

--- Tools/Chrome.cs
+++ Tools/Chrome.cs
@@ -5,24 +5,46 @@ using System.Windows.Automation;
 
 namespace HAcomms.Tools;
 
-class Chrome {
+internal static class Chrome {
     [DllImport("user32.dll")] private static extern bool IsWindowVisible(IntPtr hWnd);
-    [DllImport("user32.dll", SetLastError = true)] static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)] static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
 
-    
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    private static Dictionary<IntPtr, List<string>> _cache = new(); 
+
+
     public static List<string> GetAllTabTitles(IEnumerable<IntPtr> hWnds) {
         var tabTitles = new List<string>();
         foreach (IntPtr hWnd in hWnds) {
-            GetWindowTabs(hWnd, tabTitles);
+            GetWindowTabTitles(hWnd, tabTitles);
         }
 
         return tabTitles;
     }
-    
-    private static void GetWindowTabs(IntPtr hWnd, List<string> tabTitles) {
+
+    public static bool MatchesWatchedEntity(IntPtr hWnd, WatchedEntity we) {
+        _cache.TryGetValue(hWnd, out var titles);
+        if (titles != null) {
+            return titles.Any(we.Matches);
+        }
+        
+        titles = GetWindowTabTitles(hWnd);
+        _cache.Add(hWnd, titles);
+        return titles.Any(we.Matches);
+    }
+
+    public static void ResetCache() {
+        _cache.Clear();
+    }
+
+    private static List<string> GetWindowTabTitles(IntPtr hWnd, List<string>? tabTitles = null) {
+        List<string> titles = [];
         if (!IsWindowVisible(hWnd)) {
-            return;
+            return titles;
         }
 
         var sClassName = new StringBuilder(256);
@@ -31,9 +53,14 @@ class Chrome {
         GetClassName(hWnd, sClassName, sClassName.Capacity);
 
         //Only want visible chrome windows (not any electron type apps that have chrome embedded!)
-        if (((sClassName.ToString() == "Chrome_WidgetWin_1") && (processFromId.ProcessName == "chrome"))) {
-            FindChromeTabs(hWnd, tabTitles);
+        if (((sClassName.ToString() != "Chrome_WidgetWin_1") || (processFromId.ProcessName != "chrome"))) {
+            return titles;
         }
+
+        FindChromeTabs(hWnd, titles);
+        tabTitles?.AddRange(titles);
+
+        return titles;
     }
 
     private static void FindChromeTabs(IntPtr hWnd, List<string> tabTitles) {
@@ -43,7 +70,7 @@ class Chrome {
         var firstTab = rootElement.FindFirst(TreeScope.Descendants, condition);
         var parent = tree.GetParent(firstTab);
         var tabs = parent.FindAll(TreeScope.Children, condition);
-        
+
         foreach (AutomationElement tab in tabs) {
             string[] pieces = tab.Current.Name.Split(" - ");
             pieces = pieces.Take(pieces.Length - 2).ToArray();
